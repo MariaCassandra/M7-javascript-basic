b5b2108508f67691bead27f0efb9b75a23cbc10b
Maria Cassandra <73235842+MariaCassandra@users.noreply.github.com>
2021-02-23 15:58:45 +0100

    Create main.js

---
+++ nivel_1/main.js
@@ -0,0 +1,34 @@
+//Exercicio 1
+console.log('Hello World!');
+
+//Exercicio 2
+alert('My name is Maria!');
+
+//Exercicio 3
+const firstName = 'Maria';
+const lastName = 'Cassandra';
+console.log(firstName + ' ' + lastName);
+
+//Exercicio 4
+let num1 = 23;
+let num2 = 6;
+let result = num1 + num2;
+console.log("La suma entre "+ num1 +" y " + num2 + " es: "+ result);
+
+//Exercicio 5
+
+
+
+
+//Exercicio 6
+
+
+
+
+//Exercicio 7
+
+
+
+
+
+//Exercicio 8
